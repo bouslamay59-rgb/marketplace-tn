097e7312fcedf4d7f0e99592485e1d93f35dc4ef
bouslamay59-rgb <bouslamay59@gmail.com>
2025-10-07 21:49:55 +0100

    Create layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,9 @@
+export const metadata = { title: "Marketplace TN" };
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="fr">
+      <body>{children}</body>
+    </html>
+  );
+}
